046cde649e53dba1b098f23870e5b3d7c5a9b9f2
darch7 <monzondaniel554@gmail.com>
2020-09-24 22:58:11 -0300

    actualizacion

--- compilar/dmenu/config.h
+++ compilar/dmenu/config.h
@@ -12,9 +12,9 @@ static const unsigned int fgalpha = OPAQUE;
 static const char *prompt      = NULL;      /* -p  option; prompt to the left of input field */
 static const char *colors[SchemeLast][2] = {
 	/*     fg         bg       */
-	[SchemeNorm] = { "#2CB5C2", "#000000" },
-	[SchemeSel] = { "#000000", "#FF51DC" },
-	[SchemeOut] = { "#000000", "#FF51DC" },
+	[SchemeNorm] = { "#8C877D", "#000000" },
+	[SchemeSel] = { "#000000", "#B40A00" },
+	[SchemeOut] = { "#000000", "#B40A00" },
 };
 static const unsigned int alphas[SchemeLast][2] = {
 	/*		fgalpha		bgalphga	*/
